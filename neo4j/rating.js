bafafed9c2d99b3cbf952ad56ca49824311d767a
tacerihsehc613 <polly.kim.dev@gmail.com>
2023-05-19 15:06:28 +0900

    neo rating router added

---
+++ neo4j/rating.js
@@ -0,0 +1,123 @@
+const neo4j = require('neo4j-driver');
+
+function createDriver() {
+    const driver = neo4j.driver(
+        'neo4j+s://21f49612.databases.neo4j.io',
+        neo4j.auth.basic('neo4j', 'Jay2833748!')
+    );
+    return driver;
+}
+
+const driver = createDriver();
+const session = driver.session();
+
+const query=`
+    CALL {
+        MATCH (c:Customer_INTER)
+        WHERE c.from = $identity
+        UNWIND keys(apoc.convert.fromJsonMap(c.map)) AS c2_id
+        MATCH (c2:Customer)
+        WHERE c2.identity = toInteger(c2_id)
+        RETURN c2.identity AS c, apoc.convert.fromJsonMap(c.map)[c2_id] AS num, c2.lastname as name, c2.community as community
+        UNION ALL
+        MATCH (c:Customer_INTER)
+        WHERE toString($identity) IN keys(apoc.convert.fromJsonMap(c.map))
+        MATCH (customer:Customer {identity: c.from})
+        RETURN c.from AS c, apoc.convert.fromJsonMap(c.map)[toString($identity)] AS num, customer.lastname AS name,customer.community as community
+    }
+    return c,num,name,community
+    ORDER BY num DESC limit 3
+`;
+const query2=`
+MATCH (c1:Customer)-[b:BoughtAt]->(s:Store)
+WHERE c1.identity IN $cList AND NOT EXISTS {
+  MATCH (c2:Customer {identity: $identity})-[:BoughtAt]->(s)
+}
+RETURN c1.identity,c1.community,s.storeId, s.name, b.num
+ORDER BY b.num DESC limit 3
+`;
+const identity=7;
+
+const cList = [3487, 79, 601]
+const params = {
+    identity: neo4j.int(identity),
+    cList: cList
+};
+
+session.run(query, { identity: neo4j.int(identity) })
+    .then(result => {
+        result.records.forEach(record => {
+            const c = record.get('c');
+            const num = record.get('num');
+            const name = record.get('name');
+            const community = record.get('community');
+            console.log(`c: ${c}, num: ${num}, name: ${name}, community: ${community}`);
+        });
+    })
+    .catch(error => {
+        console.error(error);
+      })
+    .finally(() => {
+        session.close();
+    });
+
+    const session2 = driver.session(); 
+    session2.run(query2, params)
+    .then(result => {
+        result.records.forEach(record => {
+            const c = record.get('c1.identity');
+            const community = record.get('c1.community');
+            const storeId = record.get('s.storeId');
+            const name = record.get('s.name');
+            const num = record.get('b.num');
+            console.log(`c: ${c}, community: ${community}, storeId: ${storeId}, name: ${name}, num: ${num}`)
+        });
+    })
+    .catch(error => {
+        console.error(error);
+      })
+    .finally(() => {
+        session2.close();
+        driver.close();
+    });
+
+async function getNeoRecommendationUser(query, identity) {
+    const driver= createDriver();
+    const session = driver.session();
+    try {
+        const result = await session.run(query, { identity: neo4j.int(identity) });
+        const records = result.records.map(record => ({
+            c: record.get('c').toNumber(),
+            num: record.get('num').toNumber(),
+            name: record.get('name'),
+            community: record.get('community').toNumber()
+        }));
+        return records;
+    } catch (error) {
+        return Promise.reject(error);
+    }
+} 
+
+async function getNeoRecommendationStore(query, identity, cList) {
+    const driver= createDriver();
+    const session = driver.session();
+    try {
+        const params = {
+            identity: neo4j.int(identity),
+            cList: cList
+        };
+        const result = await session.run(query2, params);
+        const records = result.records.map(record => ({
+            c: record.get('c1.identity'),
+            community: record.get('c1.community').toNumber(),
+            storeId: record.get('s.storeId').toNumber(),
+            name: record.get('s.name'),
+            num: record.get('b.num').toNumber()
+        }));
+        return records;
+    } catch (error) {
+        return Promise.reject(error);
+    }
+} 
+
+module.exports = {getNeoRecommendationUser, getNeoRecommendationStore};
---
+++ routes/rating.js
@@ -0,0 +1,77 @@
+const express = require('express');
+
+const {getNeoRecommendationUser, getNeoRecommendationStore} = require('../neo4j/rating');
+const router = express.Router();
+
+router.use((req, res, next) => {
+    res.locals.user = req.user;
+    console.log("xxxxx33");
+    console.log(res.locals.user);
+    next();
+});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const query=`
+    CALL {
+        MATCH (c:Customer_INTER)
+        WHERE c.from = $identity
+        UNWIND keys(apoc.convert.fromJsonMap(c.map)) AS c2_id
+        MATCH (c2:Customer)
+        WHERE c2.identity = toInteger(c2_id)
+        RETURN c2.identity AS c, apoc.convert.fromJsonMap(c.map)[c2_id] AS num, c2.lastname as name, c2.community as community
+        UNION ALL
+        MATCH (c:Customer_INTER)
+        WHERE toString($identity) IN keys(apoc.convert.fromJsonMap(c.map))
+        MATCH (customer:Customer {identity: c.from})
+        RETURN c.from AS c, apoc.convert.fromJsonMap(c.map)[toString($identity)] AS num, customer.lastname AS name,customer.community as community
+    }
+    return c,num,name,community
+    ORDER BY num DESC limit 3
+    `;
+    const query2=`
+MATCH (c1:Customer)-[b:BoughtAt]->(s:Store)
+WHERE c1.identity IN $cList AND NOT EXISTS {
+  MATCH (c2:Customer {identity: $identity})-[:BoughtAt]->(s)
+}
+RETURN c1.identity,c1.community,s.storeId, s.name, b.num
+ORDER BY b.num DESC limit 3
+`;
+    const identity=0;
+    const users= await getNeoRecommendationUser(query, identity);
+    console.log("neo-users");
+    console.log(users);
+    console.log(typeof(users));
+    const cList = users.map(user => user.c);
+    console.log(cList);
+    console.log(typeof(cList));
+    const params = {
+        identity: identity,
+        c: cList
+    };
+    const stores= await getNeoRecommendationStore(query2,identity,cList);
+    console.log("neo-stores");
+    console.log(stores);
+    let rating;
+    console.log(req.user);
+    const user=req.user;
+    if (stores.every(store => store.community === user.community)) {
+        rating = 100;
+      } else if (stores.length === 1) {
+        rating = 33;
+      } else if (stores.length === 2) {
+        rating = 66;
+      } else {
+        rating = 0;
+      }
+      
+      console.log('Rating:', rating);
+    
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+
+module.exports = router;

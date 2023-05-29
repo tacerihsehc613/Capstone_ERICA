fb642b2193a1f73a7799b11c011a8408ad043d1b
tacerihsehc613 <polly.kim.dev@gmail.com>
2023-05-29 16:09:43 +0900

    mysql delete

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const similarRouter = require('./routes/similar');
 const ratingRouter = require('./routes/rating');
 const storeRouter = require('./routes/store');
 
-const { sequelize } = require('./models');
+//const { sequelize } = require('./models');
 const passportConfig = require('./passport');
 
 const app = express();
@@ -30,13 +30,13 @@ nunjucks.configure('views', {
     express: app,
     watch: true,
 });
-sequelize.sync({ force: false })
+/*sequelize.sync({ force: false })
     .then(() => {
         console.log('데이터베이스 연결 성공');
     })
     .catch((err) => {
         console.error(err);
-    });
+    }); */
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
